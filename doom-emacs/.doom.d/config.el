3c66ac2caada2f26c01b9b51a4967dea04501f7b
Wil Taylor <wilfridtaylor@gmail.com>
2020-02-17 18:26:01 +1000

    Added doom emacs

---
+++ doom-emacs/.doom.d/config.el
@@ -0,0 +1,84 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Wil Taylor"
+      user-mail-address "email@wiltaylor.dev")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+(setq org-brain-path "~/org/brain")
+
+(after! org-brain
+(with-eval-after-load `evil
+  (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+
+(setq org-id-track-globally t)
+(setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+(push '("b" "Brain" plain (function org-brain-goto-end)
+        "* %i%?" :empty-lines 1)
+      org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12)
+  (setq org-brain-include-file-entries nil
+        org-brain-file-entries-use-title nil))
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;;Makes copy and paste work as expected
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+
+(setq ob-mermaid-cli-path "/home/wil/.doom.d/mermaid/node_modules/.bin/mmdc")
+
+;;(require 'org)
+;;(define-key org-mode-map (kbd "Spc t m") 'org-toggle-latex-fragment)
+
+
+(map! :after org
+      :map org-mode-map
+      :localleader
+      :desc "Toggle Maths" "m" #'org-latex-preview
+      :desc "Tottlge Image" "M" #'org-display-inline-images
+      :desc "Org Brain" "B" #'org-brain-visualize)
