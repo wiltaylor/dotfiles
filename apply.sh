c74f78932ae97b7dd9e21b17fc179ec7125fd1e2
Wil Taylor <cert@wiltaylor.dev>
2020-02-19 15:49:01 +1000

    adding sub modules and tmux

---
+++ apply.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#Pulling in sub modules
+git submodule update --init --recursive
+
+#Deploying all config
+stow alacritty
+stow bspwm
+stow doom-emacs
+stow git
+stow gpg
+stow gtk
+stow i3wm
+stow kde
+stow newsboat
+stow nvim
+stow ranger
+stow rofi
+stow tmux
+stow zsh
+
