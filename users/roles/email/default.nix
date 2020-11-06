5faba87a9ecc2e14ceb7ae2abf5ada4ad345bc42
Wil Taylor <cert@wiltaylor.dev>
2020-11-06 22:32:57 +1000

    Added neomutt and protonmail

--- users/roles/email/default.nix
+++ users/roles/email/default.nix
@@ -2,7 +2,30 @@
 {
   home.packages = with pkgs; [
     neomutt
-    protonmail-bridge
-    offlineimap
+    hydroxide
   ];
+
+  systemd.user.services.hydroxide = {
+    Service = {
+      ExecStart = "${pkgs.hydroxide}/bin/hydroxide serve";
+      Restart = "always";
+      RestartSec = 6;
+    };
+
+    Unit = {
+      After = "graphical-session-pre.target";
+      Description = "Protonmail Bridge service without gui crap";
+      PartOf = "graphical-session.target";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  home.file = {
+    ".secrets/mail".source = ../../../.secret/email/mail;
+    ".config/hydroxide/auth.json".source = ../../../.secret/email/auth.json;
+    ".config/neomutt/neomuttrc".source = ./neomuttrc;
+  };
 }
