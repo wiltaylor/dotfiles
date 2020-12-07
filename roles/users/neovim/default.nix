f1068f2a308e2ef375d1c6f3a126870680f05b45
Wil Taylor <cert@wiltaylor.dev>
2020-12-07 23:23:24 +1000

    Updated nvim settings

--- roles/users/neovim/default.nix
+++ roles/users/neovim/default.nix
@@ -9,6 +9,18 @@
     python3
   ];
 
+#  home.file = {
+#    ".config/nvim/coc-settings.json".text = ''
+#      {}
+#    '';
+#  t;
+
+  #TODO: Bug with coc-explorer not installing properly.
+  # So getting nvim to install it during activation. A bit of a hack
+  home.activation.neovim = lib.hm.dag.entryAfter ["writeBoundry"] ''
+    nvim -c 'CocInstall -sync coc-explorer|q|q'
+  '';
+
   programs.neovim = {
     enable = true;
     vimAlias = true;
@@ -23,30 +35,30 @@
       pip
     ];
 
-    plugins = with pkgs.vimPlugins; [
+    plugins = with pkgs.unstable.vimPlugins; [
       nord-vim
       vim-devicons
       vimwiki
       vim-nix
+      coc-explorer
       coc-nvim
-      coc-python
+#      coc-python
       coc-tsserver
       coc-json
       coc-yaml
-      coc-wxml
-      coc-markdownlint
-      coc-vimlsp
-      coc-vimtex
-      coc-snippets
-      coc-rls
+#      coc-wxml
+#      coc-markdownlint
+#      coc-vimlsp
+#      coc-vimtex
+#      coc-snippets
+#      coc-rls
       coc-html
-      coc-jest
-      coc-go
-      coc-git
-      coc-fzf
-      coc-eslint
+#      coc-jest
+#      coc-go
+#      coc-git
+#      coc-fzf
+#      coc-eslint
       coc-css
-      coc-explorer
       fzf-vim
     ];
 
@@ -66,6 +78,82 @@
       set ai "Auto indent
       set si "Smart indent
       set pyxversion=3 "Avoid using python 2 when possible, its eol
+      set hidden " coc needs this or TextEdit can fail
+      set cmdheight=2 " for coc
+      set updatetime=300
+      set shortmess+=c
+      set signcolumn=yes
+
+      " Mapping tab complete for coc
+      inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : <SID>check_back_space() ? "\<TAB>" : coc#refresh()
+      inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+      inoremap <silent><expr> <c-space> coc#refresh() " Space to trigger completion
+      inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+      " Use `[g` and `]g` to navigate diagnostics
+      " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
+      nmap <silent> [g <Plug>(coc-diagnostic-prev)
+      nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+      " GoTo code navigation.
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)
+
+      " Highlight the symbol and its references when holding the cursor.
+      autocmd CursorHold * silent call CocActionAsync('highlight')
+
+      " Symbol renaming.
+      nmap <leader>rn <Plug>(coc-rename)
+
+      " Formatting selected code.
+      xmap <leader>f  <Plug>(coc-format-selected)
+      nmap <leader>f  <Plug>(coc-format-selected)
+
+      " Applying codeAction to the selected region.
+      " Example: `<leader>aap` for current paragraph
+      xmap <leader>a  <Plug>(coc-codeaction-selected)
+      nmap <leader>a  <Plug>(coc-codeaction-selected)
+
+
+      " Remap keys for applying codeAction to the current buffer.
+      nmap <leader>ac  <Plug>(coc-codeaction)
+      " Apply AutoFix to problem on the current line.
+      nmap <leader>qf  <Plug>(coc-fix-current)
+
+      " Map function and class text objects
+      " NOTE: Requires 'textDocument.documentSymbol' support from the language server.
+      xmap if <Plug>(coc-funcobj-i)
+      omap if <Plug>(coc-funcobj-i)
+      xmap af <Plug>(coc-funcobj-a)
+      omap af <Plug>(coc-funcobj-a)
+      xmap ic <Plug>(coc-classobj-i)
+      omap ic <Plug>(coc-classobj-i)
+      xmap ac <Plug>(coc-classobj-a)
+      omap ac <Plug>(coc-classobj-a)
+
+      nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+      nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+      inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+      inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+      vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+      vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+
+      nmap <silent> <C-s> <Plug>(coc-range-select)
+      xmap <silent> <C-s> <Plug>(coc-range-select)
+
+      command! -nargs=0 Format :call CocAction('format')
+      command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+      command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
+      " set statusline^=%{coc#status()}%{get(b:,'coc_current_function',' ')}
+
+      :nmap <space>e :CocCommand explorer<CR>
+
+      function! s:check_back_space() abort
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~# '\s'
+      endfunction
 
       " No annoying sound on errors
       set noerrorbells
