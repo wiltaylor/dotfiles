a5053b2fe03180004d195be72cac9884cf483c7c
Wil Taylor <cert@wiltaylor.dev>
2020-12-07 21:51:07 +1000

    Changed over to nerd fonts for complete icon sets

--- roles/users/desktop/shared/fonts.nix
+++ roles/users/desktop/shared/fonts.nix
@@ -1,15 +1,7 @@
 {pkgs, config, lib, ...}:
 {
   home.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    powerline-fonts
-    font-awesome
-    font-awesome-ttf
+    nerdfonts
   ];
 
   home.file = {
@@ -20,12 +12,7 @@
         <alias>
           <family>monospace</family>
           <prefer>
-            <family>DejaVu Sans Mono</family>
-            <family>Noto Color Emoji</family>
-            <family>Roboto Mono for Powerline</family>
-            <family>Noto Emoji</family>
-            <family>Font Awesome 5 Free</family>
-            <family>Font Awesome 5 Brands</family>
+            <family>Monoid Nerd Font Mono</family>
           </prefer>
         </alias>
       </fontconfig>
