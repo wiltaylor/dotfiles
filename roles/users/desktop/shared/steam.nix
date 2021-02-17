175367f6df1ca7f4f9a768cd1d1bfb1149ede543
Wil Taylor <cert@wiltaylor.dev>
2021-02-17 23:21:51 +1000

    Added newer versions of GE Proton

--- roles/users/desktop/shared/steam.nix
+++ roles/users/desktop/shared/steam.nix
@@ -3,7 +3,10 @@
   home.activation.steam-proton = lib.hm.dag.entryAfter ["writeBoundary"] ''
     mkdir ~/.steam/root/compatibilitytools.d -p
     rm ~/.steam/root/compatibilitytools.d/Proton_5.21-GE-1 -fr
+    rm ~/.steam/root/compatibilitytools.d/Proton_6.1-GE-2 -fr
+    rm ~/.steam/root/compatibilitytools.d/Proton_6.1-GE-1 -fr
     ln -fs  ${pkgs.my.proton_5_21_ge_1}/proton ~/.steam/root/compatibilitytools.d/Proton_5.21-GE-1
-
+    ln -fs  ${pkgs.my.proton_6_1_ge_2}/proton ~/.steam/root/compatibilitytools.d/Proton_6.1-GE-2
+    ln -fs  ${pkgs.my.proton_6_1_ge_1}/proton ~/.steam/root/compatibilitytools.d/Proton_6.1-GE-1
   '';
 }
