d4928fe8812cc2ca379471505155b468c6bfdef2
Wil Taylor <cert@wiltaylor.dev>
2021-02-19 21:40:33 +1000

    Moved coc-settings.json into dotfiles

--- roles/users/neovim/default.nix
+++ roles/users/neovim/default.nix
@@ -18,12 +18,12 @@ with stdenv.lib;
   home.file = {
     ".config/nvim/init.vim".source = ./init.vim;
     ".local/share/nvim/site/autoload/plug.vim".source = ./plug.vim;
+    ".config/nvim/coc-settings.json".source = ./coc-settings.json;
   };
 
 
   # So getting nvim to install it during activation. A bit of a hack
   home.activation.neovim = lib.hm.dag.entryAfter ["writeBoundry"] ''
     nvim -c 'PlugInstall|q|q'
-    #nvim -c 'CocInstall -sync coc-explorer coc-tsserver coc-json coc-html coc-css coc-yaml coc-git coc-prettier coc-angular coc-clangd coc-cmake coc-diagnostic coc-git coc-go coc-java coc-markdownlint coc-omnisharp coc-powershell coc-python coc-rls coc-sh coc-spell-checker coc-sql coc-svg coc-swagger coc-texlab coc-toml coc-xml coc-yaml |q|q'
   '';
 } 
