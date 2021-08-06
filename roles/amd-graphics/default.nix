99af20553a25d3b43c6a522dcc626d37f5821448
Wil Taylor <cert@wiltaylor.dev>
2021-08-06 18:35:40 +1000

    Added lutris and fixed amd driver settings to prevent it from trying to use closed source drivers with games

--- roles/amd-graphics/default.nix
+++ roles/amd-graphics/default.nix
@@ -6,7 +6,7 @@
 
   '';
 
-#boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
 
   hardware.opengl.extraPackages = with pkgs; [
     amdvlk 
@@ -16,29 +16,12 @@
   hardware.opengl.extraPackages32 = with pkgs; [
     driversi686Linux.amdvlk
   ];
-
-
   
-#   boot.kernelPatches = [{
-#     name = "AMD 6800XT fix";
-#     patch = null; # ./amd.patch;
-#     extraConfig = ''
-#       DRM_AMD_DC y
-#       DRM_AMD_DC_DCN y
-#       DRM_AMD_DC_DCN3_0 y
-#     '';
-#   }];
-
    environment.systemPackages = with pkgs; [
      vulkan-tools
      mesa mesa_drivers
      rocm-opencl-icd
      rocm-opencl-runtime
-     rocm-runtime
-     rocm-smi
-     rocm-thunk
-     rocm-comgr
-     rocm-device-libs
      opencl-icd
      opencl-info
      opencl-clang
@@ -52,7 +35,5 @@
   hardware.enableRedistributableFirmware = true;
   nixpkgs.config.allowUnfree = true;
   hardware.opengl.enable = true;
-
-  environment.variables.VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
 
 }
