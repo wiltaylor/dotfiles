c0ba6ac7b4506d7fee3f3d4d40ff610ab96f2f10
Wil Taylor <cert@wiltaylor.dev>
2022-01-03 15:18:00 +1000

    Cleaned up some hotfixes that have since been fixed

--- modules/hotfix/default.nix
+++ modules/hotfix/default.nix
@@ -3,19 +3,9 @@
   ## This module is a place to put hacks that need to be applied to work around current issues.
   ## You should check back here periodically to see if they can be removed.
 
-  # This is done to work around this test being flakey and causing build to fail
-  nix.package = (pkgs.nixFlakes.overrideAttrs (oldAttrs: rec {
-    preInstallCheck = ''
-      echo "exit 99" > tests/ca/substitute.sh
-    '';
-  }));
-
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
-
-  # Hot fix for issues
-  documentation.info.enable = false;
 
   environment.pathsToLink = ["/libexec" ];
 }
