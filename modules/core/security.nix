dc0c5ccc55f658f43b75da8b00d455f1fd83d335
Wil Taylor <cert@wiltaylor.dev>
2022-04-19 17:52:16 +1000

    Added gpg settings

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -24,6 +24,11 @@ in {
       description = "Store secrets in this attribute in the git crypt layer.";
       default = {};
     };
+
+    gpgKeyId = mkOption {
+      type = types.str;
+      description = "id of gpg key";
+    };
   };
 
   config = {
@@ -62,6 +67,44 @@ in {
         enableSSHSupport = true;
         pinentryFlavor = "qt";
       };
+    };
+
+    sys.users.primaryUser.files = {
+      gpg = {
+        path = ".gnupg/gpg.conf";
+        text = ''
+          personal-cipher-preferences AES256 AES192 AES
+          personal-cipher-preferences AES256 AES192 AES
+          personal-digest-preferences SHA512 SHA384 SHA256
+          personal-compress-preferences ZLIB BZIP2 ZIP Uncompressed
+          default-preference-list SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed
+          cert-digest-algo SHA512
+          s2k-digest-algo SHA512
+          s2k-cipher-algo AES256
+          charset utf-8
+          fixed-list-mode
+          no-comments
+          no-emit-version
+          keyid-format 0xlong
+          list-options show-uid-validity
+          verify-options show-uid-validity
+          with-fingerprint
+          require-cross-certification
+          no-symkey-cache
+          use-agent
+          throw-keyids
+
+          #Trust Own GPG Key
+          trusted-key ${cfg.gpgKeyId}
+          default-key ${cfg.gpgKeyId}
+        '';
+      };
+    };
+
+    system.activationScripts = {
+      gpgpubKey.text = mkIf cfg.yubikey ''
+        gpg --import ${./public.asc}
+      '';
     };
   };
 }
