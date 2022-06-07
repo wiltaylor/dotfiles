5567bba987932ad4788182eea128f5b018e73a8b
Wil Taylor <email@wiltaylor.dev>
2022-06-07 17:47:43 +1000

    feat: add zeus laptop by copying mini config

--- flake.nix
+++ flake.nix
@@ -66,13 +66,13 @@
 
           sys.desktop.usb.udisk2 = true;
           sys.desktop.kanshi.profiles = [
-            {
+            { 
               "DP-2" = "position 0,0";
               "HDMI-A-1" = "position 3840,0";
               "DP-1" = "position 7680,0";
             }
 
-            {
+            { 
               "DP-1" = "position 0,0";
             }
           ];
@@ -130,6 +130,57 @@
             fsType = "btrfs";
             options = [ "subvol=@" ];
           };
+        };
+      };
+
+      zeus = lib.mkNixOSConfig {
+        name = "zeus";
+        system = "x86_64-linux";
+        inherit nixpkgs allpkgs;
+
+        cfg = let 
+          pkgs = allPkgs.x86_64-linux;
+        in {
+          boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+
+          networking.interfaces."wlo1" = { useDHCP = true; };
+          networking.wireless.interfaces = [ "wol1" ];
+          networking.networkmanager.enable = true;
+          networking.useDHCP = false; 
+
+          sys.locale = "en_AU.UTF-8";
+          sys.timeZone = "Australia/Brisbane";
+
+          sys.users.primaryUser.extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "wil" ];
+
+          sys.desktop.usb.udisk2 = true;
+          sys.graphics.displayManager = "gdm";
+          sys.graphics.desktopProtocols = [ "xorg" "wayland" ];
+          sys.cpu.type = "intel";
+          sys.cpu.cores = 4;
+          sys.cpu.threadsPerCore = 2;
+          sys.cpu.sensorCommand = ''sensors | grep "pch_cannonlake-virtual" -A 3 | grep "temp1" | awk '{print $2}' '';
+          sys.biosType = "efi";
+          sys.graphics.primaryGPU = "intel";
+
+          sys.graphics.gpuSensorCommand = ''sensors | grep "pch_cannonlake-virtual" -A 3 | grep "temp1" | awk '{print $2}' '';
+          sys.audio.server = "pipewire";
+          sys.virtualisation.docker.enable = true;
+          sys.virtualisation.appImage.enable = true;
+
+          sys.desktop.kanshi.profiles = [
+            {
+              "eDP-1" = "position 0,0";
+            }
+          ];
+
+          sys.virtualisation.kvm.enable = true;
+          sys.bluetooth = true;
+
+          sys.security.yubikey = true;
+          sys.security.username = "wil";
+          sys.security.gpgKeyId = "0xB6840C8AFFAD67EC";
+          sys.security.sshPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2u34uDlLjo6YfpgyvYTnhsUcmlANFdXEOo+jaM9R7DxNXjTVouMX06gwXvhtoKzbzqYf4OKBe+4xPA1rj/eBQenmCtzMLLCEy8JNDtx6KqdmrAZF9zlT71Y53Kl/EFFUDLEECcy6OmjkMDBLkxG6VhE3d3P39NbfXYa606dD0c6iGhZbj3iQK08Lz0Mt/S93/dQV6AfHtQDq0I/V5UwaA6vhpqFCkdqWWDxsew6IUxVXDFLLfb/ghYt4RND6c2xq2mqSwhZ9uVjUBdju0mZfgnQ616JkRGJANuE8BRUijp6LUswz1GYA7b0B7a0nKwk+VLoy6yYj8a+AX5XuREF70IeE2Kq85KmfRnumxMfAvLFDO0i9ACGyzmwFLP/tYyYyk9T4Ttdk8PM94BrlsHcFkZ3DcAtsx4H84KaWAsaZPVC+tBQFrTVS9HdJdi09L4N5+db4Cs1Fhwm69YXcSkQvNN61g3C5lYER7U7Wc4L7l1AlqxaEBdDURpGcpAjUvlRO+ZlTyUF/ZR3Qx24jMWtK3VkZdIkaV253v4TuZcDHwHub/9MnbUMydyTsp94n50WeKpAz/PHBHeB5KpE29DWNk8vmEQ134/t4S0hc6yL0vTGmlMLLOzqC0GNBBps+yamMI9xj6GVcic152+B2+mILRPC4LQu3u5nSCRaq2Qflh1Q==";
         };
       };
 
