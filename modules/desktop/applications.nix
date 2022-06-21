3b77d1001eef5886d8e1c8074c2582c817dc3596
Wil Taylor <email@wiltaylor.dev>
2022-06-21 18:24:26 +1000

    Added jetbrains tools

--- modules/desktop/applications.nix
+++ modules/desktop/applications.nix
@@ -11,7 +11,14 @@ let
   '';
 in {
 
+
+
   config = mkIf desktopMode {
+
+  environment.sessionVariables = {
+    "_JAVA_AWT_WM_NONREPARENTING" = "1";
+  };
+
   environment.systemPackages = with pkgs; [
       pavucontrol
       pkgs.xorg.xmodmap
@@ -57,6 +64,22 @@ in {
       steam
       protonup
       jetbrains.goland
+      jetbrains.pycharm-professional
+      jetbrains.rider
+      jetbrains.jdk
+      jetbrains.clion
+      jetbrains.datagrip
+      jetbrains.ruby-mine
+      jetbrains.idea-ultimate
+      go
+      rustup
+      clang
+      gcc
+      ruby
+      python3Full
+      dotnet-sdk
+      dotnet-runtime
+      mono
     ];
   };
 }
