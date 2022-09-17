527b96e8a52c031f46a7b6935831646f41885c82
Wil Taylor <email@wiltaylor.dev>
2022-09-17 12:36:07 +1000

    fix: forced version of distrobox to work around current issue

--- modules/desktop/applications.nix
+++ modules/desktop/applications.nix
@@ -29,12 +29,22 @@ in {
       feh
       foot
       libsixel
+      mpv
 
       dfeet
       pkgs.xorg.xhost
 
       firefox
-      distrobox
+      (distrobox.overrideAttrs (old: { 
+	version = "1.4.1";
+	
+      	src = fetchFromGitHub {
+    		owner = "89luca89";
+    		repo = "distrobox";
+    		rev = "1.4.1";
+    		sha256 = "sha256-WIpl3eSdResAmWFc8OG8Jm0uLTGaovkItGAZTOEzhuE=";
+        };
+      }))
     ];
   };
 }
